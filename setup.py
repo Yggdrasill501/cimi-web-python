b27e5762cf555e247f545332e942e4ddf95b20d6
Filip Žitný <filipzitny@gmail.com>
2024-02-03 20:48:40 +0100

    initial python implementation

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, Extension
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-ext_modules = [
-    Pybind11Extension("cimi", ["app.h"]),
-]
-
 setup(
     name="Cimi-Web",
     version="0.1",
     author="Filip Žitný",
     description="The C++ micro framework for building web applications.",
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        Extension(
+            'cimi',
+            ['wrapper.cpp', 'client.cpp', 'server.cpp', 'socket.cpp'],
+            include_dirs=[pybind11.get_include()],
+            language='c++',
+            extra_compile_args=['-std=c++17'],
+        ),
+    ],
+    zip_safe=False,
 )
